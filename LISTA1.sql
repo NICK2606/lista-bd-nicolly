15f13747eed0d49a5541b749468065fc258898f5
Nicolly <nicollymoreira840@gmail.com>
2023-09-12 12:30:50 -0300

    quarto exercicio completo

--- LISTA1.sql
+++ LISTA1.sql
@@ -8,3 +8,8 @@ where autores.nascimento < '1901-01-01';
 select livros.titulo 
 from aula_db_exemplos.livros
 where livros.autor_id = 1;
+
+select alunos.nome 
+from alunos 
+inner join matriculas on alunos.id = matriculas.aluno_id 
+where curso = 'Engenharia de Software';
