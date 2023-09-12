b4bb5bfea4e0a4b01008baf2d7c1d53151489d26
Nicolly <nicollymoreira840@gmail.com>
2023-09-12 12:30:18 -0300

    terceiro exercicio completo

--- LISTA1.sql
+++ LISTA1.sql
@@ -4,3 +4,7 @@ from aula_db_exemplos.livros;
 select autores.nome 
 from aula_db_exemplos.autores 
 where autores.nascimento < '1901-01-01';
+
+select livros.titulo 
+from aula_db_exemplos.livros
+where livros.autor_id = 1;
